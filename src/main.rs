26e92639ab80b4c000058e2b2b2d4be0c22ca8de
HRusby <harrygeorger@hotmail.com>
2023-10-21 13:23:36 +0100

    feat: Changing project to a basic Nannou App type

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,26 @@ use nannou::prelude::*;
 fn main() {
     nannou::app(model)
         .update(update)
-        .simple_window(view)
         .run();
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
+struct Model {
+    _window: window::Id,
 }
 
+// Run once at the begining of the app, produces a fresh instance of Model
+fn model(app: &App) -> Model {
+    let _window = app.new_window().view(view).build().unwrap();
+    Model {_window}
+}
+
+// General type of event occurring on a time frequency (60 times per second) 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(PURPLE);
+// Presents the Model to some window
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(PLUM);
+    draw.ellipse().color(STEELBLUE);
+    draw.to_frame(app, &frame).unwrap();
 }
