6209a93e7b43e0c37d2b09452e2a1f0dc019f987
HRusby <harrygeorger@hotmail.com>
2023-11-12 17:00:22 +0000

    fix: Extending PerlinNoise model to be animated

--- src/models/perlin_noise.rs
+++ src/models/perlin_noise.rs
@@ -26,7 +26,7 @@ impl NannouModel for PerlinNoise{
         let boundary = app.window_rect();
         let window_x = boundary.w();
         let window_y = boundary.h();
-        for i in 0..50{
+        for _i in 0..500{
             
             let point = vec2(
                 (random::<f32>()-0.5)*window_x,
@@ -38,19 +38,28 @@ impl NannouModel for PerlinNoise{
         PerlinNoiseModel {_window, points, noise}
     }
 
-    fn update(_app: &nannou::App, _model: &mut Self::Model, _update: nannou::prelude::Update) {}
+    fn update(_app: &nannou::App, model: &mut Self::Model, _update: nannou::prelude::Update) {
+        let ns = 0.01;
+        for point in model.points.iter_mut(){
+            let scaled_point = [ns*point.x as f64, ns*point.y as f64];
+            *point += vec2(
+                model.noise.get([scaled_point[0], scaled_point[1], 0.0]) as f32,
+                model.noise.get([scaled_point[0], scaled_point[1], -1.0]) as f32
+            );
+        }
+    }
 
     fn view(app: &nannou::App, model: &Self::Model, frame: nannou::Frame) {
 
         let draw = app.draw(); 
-        let time = app.elapsed_frames() as f32 ;
-        let boundary = app.window_rect();
+        let _time = app.elapsed_frames() as f32 ;
+        let _boundary = app.window_rect();
 
 
 
         // draw.rect().w_h(boundary.w(), boundary.h()).color(srgba(0.0, 0.0, 0.0, fade_factor));
         for point in model.points.iter(){
-            draw.ellipse().xy(*point).radius(3.0).color(WHITE);
+            draw.ellipse().xy(*point).radius(1.0).color(srgba(255.0, 255.0, 255.0, 0.5));
         }
         draw.to_frame(app, &frame).unwrap();
     }
