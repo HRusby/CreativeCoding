7ed31063a27f93624700f27e670b6348bfd2453c
HRusby <harrygeorger@hotmail.com>
2023-11-12 17:29:18 +0000

    fix: Experimenting with parameters to PerlinNoise

--- src/models/perlin_noise.rs
+++ src/models/perlin_noise.rs
@@ -2,10 +2,25 @@ use nannou::prelude::*;
 use nannou::noise::*;
 use super::nannou_model::NannouModel;
 
+pub struct Thing{
+    position: Vec2,
+    color: Srgba,
+    radius: f32,
+}
+impl Thing{
+    pub fn new(p:Vec2, c:Srgba, r:f32) -> Self{
+        Thing {
+            position:p,
+            color:c,
+            radius:r,
+        }
+    }
+}
+
 pub struct PerlinNoise;
 pub struct PerlinNoiseModel{
     _window: window::Id,
-    points: Vec<Vec2>,
+    points: Vec<Thing>,
     noise: Perlin
 }
 
@@ -28,23 +43,28 @@ impl NannouModel for PerlinNoise{
         let window_y = boundary.h();
         for _i in 0..500{
             
-            let point = vec2(
-                (random::<f32>()-0.5)*window_x,
-                (random::<f32>()-0.5)*window_y
+            let point = Thing::new(
+                vec2(
+                    (random::<f32>()-0.5)*window_x,
+                    (random::<f32>()-0.5)*window_y
+                ),
+                srgba(255.0*random::<f32>()+1.0, 255.0*random::<f32>()+1.0, 255.0*random::<f32>()+1.0, 255.0),
+                random::<f32>()+0.25*2.0,
             );
             points.push(point);
         }
         let noise = Perlin::new();
+        noise.set_seed(10);
         PerlinNoiseModel {_window, points, noise}
     }
 
     fn update(_app: &nannou::App, model: &mut Self::Model, _update: nannou::prelude::Update) {
         let ns = 0.01;
         for point in model.points.iter_mut(){
-            let scaled_point = [ns*point.x as f64, ns*point.y as f64];
-            *point += vec2(
+            let scaled_point = [ns*point.position.x as f64, ns*point.position.y as f64];
+            point.position += vec2(
                 model.noise.get([scaled_point[0], scaled_point[1], 0.0]) as f32,
-                model.noise.get([scaled_point[0], scaled_point[1], -1.0]) as f32
+                model.noise.get([scaled_point[0], scaled_point[1], 0.5]) as f32
             );
         }
     }
@@ -56,10 +76,11 @@ impl NannouModel for PerlinNoise{
         let _boundary = app.window_rect();
 
 
-
-        // draw.rect().w_h(boundary.w(), boundary.h()).color(srgba(0.0, 0.0, 0.0, fade_factor));
         for point in model.points.iter(){
-            draw.ellipse().xy(*point).radius(1.0).color(srgba(255.0, 255.0, 255.0, 0.5));
+            draw.ellipse()
+                .xy(point.position)
+                .radius(point.radius)
+                .color(point.color);
         }
         draw.to_frame(app, &frame).unwrap();
     }
